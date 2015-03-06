152d6537d8ab8844e7aa2ce026411fea0d743fdc
Yishuo Wang <yishuo@ualberta.ca>
2015-03-05 18:22:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,107 @@
 #DATABASE MAIN
 #default
+
+import sys
+import cx_Oracle
+import getpass
+
+def connect():
+    '''
+    connstr = input("Enter your user name: ")
+    connstr += '/' + getpass.getpass("Enter your password: ")
+    connstr += '@gwynne.cs.ualberta.ca:1521/crs'
+    try:
+        print("Connection successful")
+        connection = cx_Oracle.connect(connstr)
+    except cx_Oracle.DatabaseError as exc:
+        print("Unable to connect to database")
+        error, = exc.args
+        print(sys.stderr, "Oracle code:". error.code)
+        print(sys.stderr, "Oracle message:", error.message)
+    return connection, connection.cursor()
+    '''
+    logintime = 0
+    while logintime < 3:
+        connstr = input("Enter youe username: ")
+        if connstr.lower() == 'q' or connstr.lower() == "quit":
+            return 0, 0
+        connstr += '/' + getpass.getpass("Enter your password: ")
+        connstr += '@gwynne.cs.ualberta.ca:1521/crs'
+        try:
+            connection = cx_Oracle.connect(connstr)
+            print("Connection successful")
+            return connection, connection.cursor()
+        except:
+            print("Unable to connect to the database")
+            logintime += 1
+            continue
+    print("Unable to connect to ORACLE after 3 attempts, exit the program")
+    return 0, 0
+
+
+def newVehicleRegistration():
+    pass
+
+
+def autoTransaction():
+    pass
+
+
+def driverLicenceRegistration():
+    pass
+
+
+def violationRecord():
+    pass
+
+
+def searchEngine():
+    pass
+
+
+# will call setup function somewhere in the main function
+def main():
+    connection, curs = connect()
+    if connection == 0:
+        print("Bye")
+        return
+    print("1. New Vehicle Registration (n)")
+    print("2. Auto Transaction (a)")
+    print("3. Driver Licence Registration (d)")
+    print("4. Violation Record (v)")
+    print("5. Search Engine (s)")
+
+    while True:
+        inp = input("Enter your choice: ").lower()
+        if inp == 'q' or inp == 'quit' or inp == "exit":
+            while True:
+                inp = input("Do you want to exit? ").lower()
+                if inp == 'y' or inp == 'yes' or inp == 'q':
+                    print("Quit")
+                    return
+                elif inp == 'n' or inp == 'no':
+                    break
+                else:
+                    print("Please enter yes (y) or no (n)")
+                    continue
+            continue
+        elif inp == '1' or inp == 'n':
+            newVehicleRegistration()
+        elif inp == '2' or inp == 'a':
+            autpTransaction()
+        elif inp == '3' or inp == 'd':
+            driverLicenceRegistration()
+        elif inp == '4' or inp == 'v':
+            violationRecord()
+        elif inp == '5' or inp == 's':
+            searchEngine()
+        else:
+            print("Invalid")
+        print(inp)
+
+    cursor.close()
+    connection.close()
+    return
+
+if __name__ == "__main__":
+    main()
