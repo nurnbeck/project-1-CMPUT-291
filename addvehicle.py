67734f55c803501e47b48f1552381162c8603027
Yishuo Wang <yishuo@ualberta.ca>
2015-03-27 12:04:45 -0600

    Update addvehicle.py

--- addvehicle.py
+++ addvehicle.py
@@ -4,26 +4,34 @@ import cx_Oracle
 def addvehicle(curs, connection, serial_no = 'zzzzzz'):
     if serial_no == 'zzzzzz':
         serial_no = input("input serial_no > ")
-        while(len(serial_no) > 15):
-            serial_no = input("input too long, input serial_no > ")
+        while(len(serial_no) > 15 or len(serial_no) == 0):
+            serial_no = input("invalid input, input serial_no > ")
         
     maker = input("input maker > ")
-    while(len(maker) > 20):
-        maker = input("input too long, input maker > ")            
+    while(len(maker) > 20 or len(maker) == 0):
+        maker = input("invalid input, input maker > ")            
         
     model = input("input model > ")
-    while(len(serial_no) > 20):
-        model = input("input too long, model > ")   
+    while(len(serial_no) > 20 or len(serial_no) == 0):
+        model = input("invalid input, model > ")   
         
     year = input("input year > ")
-    while(len(year) > 4):
+    while(len(year) > 4 or len(year) == 0):
         year = input("input after 9999, invalid year > ")                   
     color = input("input color > ")
-    while(len(color) > 10):
-        color = input("input too long, color > ")                   
+    while(len(color) > 10 or len(color) == 0):
+        color = input("invalid input, color > ")                   
     type_id = input("input type_id > ")
-    while(len(type_id) > 20):
-        type_id = input("input too long, type_id > ")         
+    
+    #while(len(type_id) > 20 or len(type_id) == 0):
+    # tupe_id integer
+    while True:
+        try:
+            type_id = int(type_id)
+            break
+        except:
+            type_id = input("invalid input, type_id > ")
+            continue
     #insertion
     query = """insert into people values(
     :serial_no,
